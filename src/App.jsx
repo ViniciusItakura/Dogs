d6426af8c27c6198a1f8f1c63205f89df67242b1
ViniciusItakura <viniciusitakura1@gmail.com>
2024-03-07 12:23:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import './App.css';
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return <div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login/*" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </div>;
+}
+
+export default App
