6fcf1471b753d4fafd497d885d192f2d1fdc3390
ViniciusItakura <viniciusitakura1@gmail.com>
2024-03-13 15:18:14 -0300

    Adicionado mais coisas

--- src/api.jsx
+++ src/api.jsx
@@ -6,9 +6,21 @@ export function TOKEN_POST(body) {
         options: {
             method: 'POST',
             headers: {
-                'Content-Type': 'aplication/json',
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify(body),
+        },
+    };
+}
+
+export function TOKEN_VALIDATE_POST(token) {
+    return {
+        url: API_URL + '/jwt-auth/v1/token/validate',
+        options: {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
         },
     };
 }
